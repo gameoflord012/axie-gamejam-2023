1bffb04be704dde310718dc6a2993334c614e6bd
nshuan <nshuan8@gmail.com>
2023-10-26 09:13:09 +0700

    item cooldown #2

--- Assets/_Huan/_Scripts/GameScene/ItemBar/ItemSlot.cs
+++ Assets/_Huan/_Scripts/GameScene/ItemBar/ItemSlot.cs
@@ -10,9 +10,6 @@ public class ItemSlot : AbstractItemSlot
         if (m_ItemSlotQuery.IsSelectable() == true)
         {
             ItemPlacingManager.Instance.SelectItemSlot(this, false);
-            cooldown = m_ItemSlotQuery.GetCooldownDuration();
-            m_ItemSlotQuery.SetSelectable(false);
-            StartCoroutine(CooldownCoroutine());
         }
     }
 }
