21ead7c745f08b3de662a986b7db7d139ea0247b
nshuan <nshuan8@gmail.com>
2023-10-29 13:55:13 +0700

    final

--- Assets/_Huan/_Scripts/GameScene/HealthBar/CharacterExpBar.cs
+++ Assets/_Huan/_Scripts/GameScene/HealthBar/CharacterExpBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CharacterHealthBar : MonoBehaviour
+public class CharacterExpBar : MonoBehaviour
 {
     private ICharacterUIQuery m_CharacterUIQuery;
 
