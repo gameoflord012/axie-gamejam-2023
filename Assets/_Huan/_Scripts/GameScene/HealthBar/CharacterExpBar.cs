39035c9a67e539525b8c3cf859b75e4d235208cd
nshuan <nshuan8@gmail.com>
2023-10-29 13:45:42 +0700

    .

---
+++ Assets/_Huan/_Scripts/GameScene/HealthBar/CharacterExpBar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterHealthBar : MonoBehaviour
+{
+    private ICharacterUIQuery m_CharacterUIQuery;
+
+    [SerializeField] private Slider healthSlider;
+
+    private void Start()
+    {
+        m_CharacterUIQuery = transform.FindSibling<ICharacterUIQuery>();
+    }
+
+    private void Update()
+    {
+        healthSlider.maxValue = m_CharacterUIQuery.GetMaxHealth();
+        healthSlider.value = m_CharacterUIQuery.GetCurrentHealth();
+
+        if (healthSlider.value <= 0)
+        {
+            healthSlider.gameObject.SetActive(false);
+        }
+    }
+}
